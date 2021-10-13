3351c8c77f9e4ffd0feaece1543448dd7f020eb3
alex <alex.pedchenko@gmail.com>
2021-10-13 15:41:58 +0300

    added firebaseApp sample

--- firebase/firebaseApp.sample.js
+++ firebase/firebaseApp.sample.js
@@ -9,13 +9,13 @@ import 'firebase/storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDBNwlNCBUi3f2ZykKYkuS8h_uXvUKU68E',
-  authDomain: 't-ukraine.firebaseapp.com',
-  projectId: 't-ukraine',
-  storageBucket: 't-ukraine.appspot.com',
-  messagingSenderId: '1049075273273',
-  appId: '1:1049075273273:web:1066884ccca3d1fc24c7e8',
-  measurementId: 'G-G5ZK52HGYL',
+  apiKey: 'apiKey',
+  authDomain: 'authDomain',
+  projectId: 'projectId',
+  storageBucket: 'storageBucket',
+  messagingSenderId: 'messagingSenderId',
+  appId: 'appId',
+  measurementId: 'measurementId',
 }
 
 // что бы избежать двойной инициализации firebase
